b4ae515c85b23f34c0398ab6435a5e31ce958498
leonardo-schneider2 <leocoelho256@gmail.com>
2022-06-21 19:24:13 -0300

    Projeto Finalizado

---
+++ src/components/Missions.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+
+        {
+          missions.map((mission) => {
+            const { name, year, country, destination } = mission;
+            return (
+              <MissionCard
+                key={ name }
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default Missions;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="Planetas" />
+        <Title
+          className="title"
+          headline="Planetas"
+        />
+
+        <div id="planets">
+          {
+            planets.map((planetObj) => {
+              const { name, image } = planetObj;
+
+              return (
+                <PlanetCard
+                  key={ name }
+                  planetName={ name }
+                  planetImage={ image }
+                  className="planet"
+                />
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
